0e376bbd8cf511b8d679114928ad5b48109e5a6e
Saravanan raghul <saravananraghul1820@gmail.com>
2020-09-25 20:07:33 +0530

    added git url

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const cacheName = "first-pwa";
-const filesToCache = ["/", "/index.html", "/css/style.css", "/js/main.js"];
+const filesToCache = ["https://saravananvijayamuthu.github.io/PWA/", "/index.html", "/css/style.css", "/js/main.js"];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener("install", (e) => {
